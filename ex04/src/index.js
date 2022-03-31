826a606f9f542263f2961cdfb0c8a4ce3c1d52fb
siriuska <you@example.com>
2022-03-31 15:39:58 +0200

    Decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  // only change code bellow this line
+  var myDecimal = 8.2;
+  var myDecimalE = 254e-5;
+
+  // only change code above this line
+  return {
+    myDecimal,
+    myDecimalE,
+  };
+}
+
+console.log(main());
+module.exports = main;
